2c5ef8feef020400d66a6db36820e198d7e6081c
sakibit17043 <it17043@mbstu.ac.bd>
2022-04-12 04:21:07 +0600

    local storage added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax =parseFloat(total* 0.1);
+    const grandTotal = total+shipping+tax;
     return (
         <div className='cart'>
              <h4>Order Summary</h4>
              <p>Selected Items: {cart.length}</p>
+             <p>Total Price:${total}</p>
+             <p>Total Shipping:${shipping}</p>
+             <p>Tax:${tax.toFixed(2)}</p>
+             <h5>Grand Total:${grandTotal.toFixed(2)}</h5>
+
+
             
                 
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import {addToDb} from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -14,10 +15,13 @@ const Shop = () => {
     }, []);
 
     const handleAddToCart = (product) =>{
-        console.log(product);
+        // console.log(product);
         // do not do this: cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
+        
+        
     }
 
     return (
