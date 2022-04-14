944200166ac288fd9b623d622755e9998254da53
sakibit17043 <it17043@mbstu.ac.bd>
2022-04-15 03:06:26 +0600

    cart calculation completed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,24 +2,57 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import {addToDb} from '../../utilities/fakedb';
+import {addToDb, getStoredCart} from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
 
     useEffect( () =>{
+        // console.log('before products added')
         fetch('products.json')
         .then(res=> res.json())
         .then(data => setProducts(data))
     }, []);
+    useEffect(()=>{
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        // console.log(products)
+        for(const id in storedCart){
+            
+            const addedProduct = products.find(product => product.id ===id);
+            // console.log(addedProduct.quantity)
 
-    const handleAddToCart = (product) =>{
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+
+            }
+        }
+        setCart(savedCart);
+    
+    
+    },[products])
+    
+
+    const handleAddToCart = (selectProduct) =>{
         // console.log(product);
         // do not do this: cart.push(product);
-        const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(product =>product.id === selectProduct.id);
+        if(!exists){
+            selectProduct.quantity = 1;
+            newCart = [...cart,selectProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+         
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectProduct.id);
         
         
     }
